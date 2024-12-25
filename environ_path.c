2cfbaf027e6d3ea1ee3a171cad366154caeef2c4
Nigel Feng <10083@holbertonstudents.com>
2024-12-25 09:24:29 +0000

    Updated

--- environ_path.c
+++ environ_path.c
@@ -16,7 +16,6 @@ int print_env(void)
     }
   return (1);
 }
-
 /**
  * _getenv - Function searches the environment list to find the
  *           environment variable name, and returns a pointer to the
@@ -26,7 +25,7 @@ int print_env(void)
  * Return: a pointer to the value in the
  *         environment, or NULL if there is no match.
  */
-char *_getenv(const char *name)
+char *_getenv(const char *env_var)
 {
   int i = 0;
   char *token;
@@ -34,12 +33,41 @@ char *_getenv(const char *name)
   while(environ[i] != NULL)
     {
       token = strtok(environ[i], "=");
-      if (strcmp(token, name) == 0)
+      if (strcmp(token, env_var) == 0)
 	{
-	  token = strtok(NULL, "=");
+	  token = strtok(NULL, "\n");
 	  return (token);
 	}
       i = i + 1;
+    }
+  return (NULL);
+}
+/**
+ * _getcommand - Function gets the environment path and checks file exists.
+ * @command: Path to examine.
+ *
+ * Return: environment path or NULL if path is not verified.
+ */
+char *_getcommand(char *command)
+{
+  char *path, *token, *cmd;
+  struct stat st;
+
+  path = _getenv("PATH");
+  token = strtok(path, ":");
+
+  while (token != NULL)
+    {
+      cmd = malloc(strlen(cmd) + strlen(token) + 2);
+      strcpy(cmd, token);
+      strcat(cmd, "/");
+      strcat(cmd, command);
+      if (stat(cmd, &st) == 0)
+	{
+	  return (cmd);
+	}
+      free(cmd);
+      token = strtok(NULL, ":");
     }
   return (NULL);
 }
