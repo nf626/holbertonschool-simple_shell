7488e5a117ccfa315449682fcf7c39f08325e0e2
Nigel Feng <10083@holbertonstudents.com>
2024-12-26 08:44:17 +0000

    Updated

--- fork_process.c
+++ fork_process.c
@@ -11,34 +11,33 @@ int fork_process(char **argv)
   pid_t child_pid;
   int status;
 
-  child_pid = fork();
-  if (child_pid == 0)
+  char *cmd = get_command(argv[0]);
+  if (!cmd)
     {
-      char *cmd = get_command(argv[0]);
-      if (cmd)
-	{
-	  execve(cmd, argv, environ);
-	}
-      else
-	{
-	  printf("Command not found\n");
-	}
-      if (execve(cmd, argv, environ) == -1)
-	{
-	  perror("./shell");
-	  exit(EXIT_FAILURE);
-	}
-      exit(2);
-    }
-  else if (child_pid < 0)
-    {
-      perror("Error fork failed");
+      printf("Command not found\n");
     }
   else
     {
-      do {
-	waitpid(child_pid, &status, WUNTRACED);
-      } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+      child_pid = fork();
+      if (child_pid == 0)
+	{
+	  if (execve(cmd, argv, environ) == -1)
+	    {
+	      perror("./shell");
+	      exit(EXIT_FAILURE);
+	    }
+	  exit(2);
+	}
+      else if (child_pid < 0)
+	{
+	  perror("Error fork failed");
+	}
+      else
+	{
+	  do {
+	    waitpid(child_pid, &status, WUNTRACED);
+	  } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
     }
   return (0);
 }
