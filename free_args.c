e19f7f7f5b0fcb4897317993b34d7ad61e876614
root <chak8x8@gmail.com>
2024-12-30 08:56:10 +0000

    free args

--- free_args.c
+++ free_args.c
@@ -6,17 +6,13 @@
  */
 void free_args(char **args)
 {
-	int i;
-	
-	if (args == NULL)
-	{
-		return;
-	}
-	
-	for (i = 0; args[i]; i++)
-	{
-		free(args[i]);
-	}
-	
-	free(args);
+    int i;
+
+    if (!args)
+        return;
+
+    for (i = 0; args[i]; i++)
+        free(args[i]);
+    free(args);
+    args = NULL; // Avoid accidental reuse
 }
