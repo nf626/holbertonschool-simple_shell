db28e5b068c28da5d564122e7d8c66112ee58420
root <chak8x8@gmail.com>
2024-12-30 08:03:27 +0000

    fix bug

--- read_input.c
+++ read_input.c
@@ -7,25 +7,25 @@
  */
 char *read_input(void)
 {
-	char *buf = NULL;
-	size_t count = 0;
-	ssize_t nread;
+    char *buf = NULL;
+    size_t count = 0;
+    ssize_t nread;
 
-	nread = getline(&buf, &count, stdin);
+    nread = getline(&buf, &count, stdin);
 
-	if (nread == -1)
-	{
-		if (!buf || buf[0] == '\0') /* Detect EOF (Ctrl+D) */
-		{
-			free(buf);
-			write(STDOUT_FILENO, "\n", 1); /* Gracefully exit with newline */
-			fprintf(stderr, "DEBUG: EOF detected.\n");
-			return (NULL);
-		}
-		free(buf);
-		perror("Exiting shell");
-		exit(1);
-	}
+    if (nread == -1)
+    {
+        if (buf)
+        {
+            free(buf);
+            write(STDOUT_FILENO, "\n", 1); /* Gracefully exit with newline */
+        }
+        else
+        {
+            perror("Exiting shell");
+        }
+        return (NULL);
+    }
 
-	return (buf);
+    return (buf);
 }
