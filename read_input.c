730af71de3b03927f364a1e044372107e77316f8
root <chak8x8@gmail.com>
2024-12-30 13:09:06 +0000

    2. Simple shell 0.1

--- read_input.c
+++ read_input.c
@@ -7,39 +7,41 @@
  */
 char *read_input(void)
 {
-    char *buf = NULL;
-    size_t count = 0;
-    ssize_t nread, i;
+	char *buf = NULL;
+	size_t count = 0;
+	ssize_t nread;
+	/* ssize_t i; */
 
-    nread = getline(&buf, &count, stdin);
+	nread = getline(&buf, &count, stdin);
 
-    if (nread == -1)
-    {
-        if (buf)
-        {
-            free(buf);
-            write(STDOUT_FILENO, "\n", 1); /* Gracefully exit with newline */
-        }
-        else
-        {
-            perror("Exiting shell");
-        }
-        return (NULL);
-    }
+	if (nread == -1)
+	{
+		if (buf)
+		{
+			free(buf);
+			write(STDOUT_FILENO, "\n", 1); /* Gracefully exit with newline */
+		}
+		else
+		{
+			perror("Exiting shell");
+		}
+		return (NULL);
+	}
 
-    /* Debug: Log the input read from stdin */
-    fprintf(stderr, "DEBUG: Input read: '%s', Length: %ld\n", buf, nread);
+	/* Debug: Log the input read from stdin */
+	/* fprintf(stderr, "DEBUG: Input read: '%s', Length: %ld\n", buf, nread); */
 
-    /* Debug: Log each character in the input */
-    for (i = 0; i < nread; i++)
-    {
-        fprintf(stderr, "DEBUG: buf[%lu]: '%c' (ASCII: %d)\n", i, buf[i], buf[i]);
-    }
-    /* Trim trailing newline and spaces */
-    trim_trailing_newline_and_spaces(buf);
+	/* Debug: Log each character in the input */
+	/* for (i = 0; i < nread; i++)
+	 * {
+	 * fprintf(stderr, "DEBUG: buf[%lu]: '%c' (ASCII: %d)\n", i, buf[i], buf[i]);
+	 * } */
 
-    /* Debug statement to verify the trimmed input */
-    fprintf(stderr, "DEBUG: Trimmed input: '%s', Length: %lu\n", buf, strlen(buf));
+	/* Trim trailing newline and spaces */
+	trim_trailing_newline_and_spaces(buf);
 
-    return (buf);
+	/* Debug statement to verify the trimmed input */
+	/* fprintf(stderr, "DEBUG: Trimmed input: '%s', Length: %lu\n", buf, strlen(buf)); */
+
+	return (buf);
 }
