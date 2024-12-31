81980cdf3ac1e8c580886301965528b71dbbea70
Nigel Feng <10083@holbertonstudents.com>
2024-12-31 07:20:47 +0000

    Betty

--- interactive.c
+++ interactive.c
@@ -5,18 +5,18 @@
  */
 void interactive(void)
 {
-  char *lineptr = NULL;
-  char **argv;
-  int status = 1;
+char *lineptr = NULL;
+char **argv;
+int status = 1;
 
-  do {
-    printf("#cisfun$ ");    
-    lineptr = read_input(); /** Read line */
-    argv = parse_line(lineptr); /** Splits line */
-    status = execute(argv); /** returns int value to determine do-while loop */
-    
-    /** free memory */
-    free(lineptr);
-    free(argv);
-  } while (status == 1);
+do {
+printf("#cisfun$ ");    
+lineptr = read_input(); /** Read line */
+argv = parse_line(lineptr); /** Splits line */
+status = execute(argv); /** returns int value to determine do-while loop */
+
+/** free memory */
+free(lineptr);
+free(argv);
+} while (status == 1);
 }
--- shell_main.c
+++ shell_main.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-  while (1)
-    {
-      if (isatty(STDIN_FILENO) == 1)
-	{
-	  interactive();
-	}
-      else
-	{
-	  non_interactive();
-	}
-    }
+while (1)
+{
+if (isatty(STDIN_FILENO) == 1)
+{
+interactive();
+}
+else
+{
+non_interactive();
+}
+}
 return (0);
 }
