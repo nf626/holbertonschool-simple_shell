3bc41a7e83313ac6a82e3871779edeb2205baaee
Nigel Feng <10083@holbertonstudents.com>
2024-12-31 01:51:46 +0000

    Testing new format

--- shell.h
+++ shell.h
@@ -8,20 +8,9 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-<<<<<<< HEAD
-/* Extern declarations */
-=======
 /** Global environment */
->>>>>>> nigel_shell
 extern char **environ;
 
-<<<<<<< HEAD
-/* Function prototypes */
-char *read_line(void);
-char **parse_line(char *lineptr);
-int fork_process(char **argv);
-unsigned int ss_num(void);
-=======
 /** shell_main.c */
 void interactive(void);
 void non_interactive(void);
@@ -38,7 +27,6 @@ int fork_process(char *lineptr, char *argv[]);
 extern char *builtin_list[];
 extern int (*builtin_func[])(char **);
 int ss_num(void);
->>>>>>> nigel_shell
 int ss_exit(char **argv);
 int ss_env(char **argv);
 
@@ -47,13 +35,5 @@ int print_env(void);
 char *_getenv(char *env_var);
 char *get_command(char *command);
 int _setenv(const char *name, const char *value, int overwrite);
-
-/* Helper functions for string manipulation */
-void _strcpy(char *dest, const char *src);
-int _strcmp(const char *str1, const char *str2);
-char *_strdup(const char *src);
-size_t _strlen(const char *str);
-void manual_strcat(char *dest, const char *src);
-char *construct_path(const char *directory, const char *command);
 
 #endif
--- shell_builtin.c
+++ shell_builtin.c
@@ -1,29 +1,5 @@
 #include "shell.h"
 
-<<<<<<< HEAD
-/** Function declaration */
-int ss_ls(char **argv);
-int ss_exit(char **argv);
-int ss_env(char **argv);
-int ss_printenv(char **argv);
-int ss_echo(char **argv);
-
-/** List of builtin commands, followed by their corresponding functions. */
-char *ss_str[] = {
-	"ls",
-	"exit",
-	"env",
-	"printenv",
-	"echo"
-};
-
-int (*ss_func[])(char **) = {
-	&ss_ls,
-	&ss_exit,
-	&ss_env,
-	&ss_printenv,
-	&ss_echo
-=======
 /**
  * builtin_list - Pointer to list of strings.
  * 
@@ -41,7 +17,6 @@ char *builtin_list[] = {
 int (*builtin_func[])(char **) = {
   &ss_exit,
   &ss_env
->>>>>>> nigel_shell
 };
 /**
  * ss_num - Get the number of characters in string.
@@ -50,49 +25,6 @@ int (*builtin_func[])(char **) = {
  */
 int ss_num(void)
 {
-<<<<<<< HEAD
-	return (sizeof(ss_str) / sizeof(char *));
-}
-
-/** Builtin function implementation */
-/** list contents of a directory */
-int ss_ls(char **argv)
-{
-	(void)argv;
-	pid_t child_pid, wpid;
-	int status;
-
-	child_pid = fork();
-	if (child_pid == 0)
-	{
-		char *ls_input[] = {"ls", NULL};
-		if (execve("/bin/ls", ls_input, environ) == -1)
-		{
-			perror("Error:");
-		}
-		exit(EXIT_FAILURE);
-	}
-	else if (child_pid < 0)
-	{
-		perror("Error:");
-	}
-	else
-	{
-		do {
-			wpid = waitpid(child_pid, &status, WUNTRACED);
-			(void)wpid;
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status)); 
-	}
-	
-	return (1);
-}
-/** exit */
-int ss_exit(char **argv)
-{
-	(void)argv;
-	exit(EXIT_SUCCESS);
-	return (0);
-=======
   return (sizeof(builtin_list) / sizeof(char *));
 }
 /** 
@@ -108,7 +40,6 @@ int ss_exit(char **argv)
       exit(EXIT_SUCCESS);
     }
       return (0);
->>>>>>> nigel_shell
 }
 /** 
  * ss_env - Print shell environment.
@@ -118,50 +49,9 @@ int ss_exit(char **argv)
 */
 int ss_env(char **argv)
 {
-<<<<<<< HEAD
-	unsigned int i = 0;
-	(void)argv;
-	int len = 0;
-	while (environ[i] != NULL)
-	{
-		while (environ[i][len] != '\0')
-		{
-			len++;
-		}
-		
-		write(STDOUT_FILENO, environ[i], len);
-		
-		write(STDOUT_FILENO, "\n", 1);
-		
-		i++;
-	}
-	return (1);
-}
-/** print "printenv" */
-int ss_printenv(char **argv)
-{
-	ss_env(argv);
-	return (1);
-}
-
-/* print "echo" */
-int ss_echo(char **argv)
-{
-	for (int i = 1; argv[i] != NULL; i++) /* Start from 1 to skip "echo" */
-	{
-		write(STDOUT_FILENO, argv[i], strlen(argv[i]));
-		if (argv[i + 1] != NULL)
-		{
-			write(STDOUT_FILENO, " ", 1); /* Add a space between arguments */
-		}
-	}
-	write(STDOUT_FILENO, "\n", 1); /* Newline at the end */
-	return (1);
-=======
   if (strcmp(argv[0], "env") == 0)
     {
       print_env();
     }
   return (0);
->>>>>>> nigel_shell
 }
